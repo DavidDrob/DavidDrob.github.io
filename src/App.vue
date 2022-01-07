de0045ca2575ca3d1acc611fccc626be939467c3
daviddrob <david.drobnak26@gmail.com>
2022-01-07 21:03:16 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div id="app" class="bg-gray-50 dark:bg-neutral-900 h-screen flex">
+    <Links />
+    <div
+      class="flex w-10/12 h-4/6 mr-24 m-auto text-neutral-900 dark:text-white"
+    >
+      <div id="hero" class="font-serif">
+        <h1 class="text-6xl font-bold">Hey! 🙋‍♂️</h1>
+        <h2 class="text-2xl mt-16">
+          I am David.<br />
+          A
+          <span class="text-neutral-500 font-semibold dark:text-zinc-400"
+            >high-school computer science student</span
+          ><br />
+          building
+          <span class="text-neutral-500 font-semibold dark:text-zinc-400"
+            >web apps.</span
+          >
+        </h2>
+        <hr class="mt-4" />
+        <h2 class="mt-8 text-lg">
+          🌱 Currently learning
+          <a href="https://vuejs.org/" target="_blank">
+            <span class="border-b-2 border-green-600">Vue.JS</span>
+          </a>
+          with
+          <a href="https://tailwindcss.com/" target="_blank">
+            <span class="border-b-2 border-teal-500">TailwindCSS</span>
+          </a>
+        </h2>
+        <!-- <h2 class="mt-4 text-lg">
+          📍 Looking for an internship either remote or in vienna
+        </h2> -->
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Links from "./components/Links.vue";
+
+export default {
+  name: "App",
+  components: {
+    Links,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
---
+++ src/components/Links.vue
@@ -0,0 +1,250 @@
+<template>
+  <nav
+    class="
+      absolute
+      inset-y-0
+      left-0
+      h-3/5
+      m-auto
+      rounded-xl
+      p-5
+      ml-6
+      flex flex-col
+      items-center
+    "
+  >
+    <div
+      class="
+        text-white
+        bg-neutral-800
+        dark:bg-gray-50
+        w-14
+        h-14
+        mb-4
+        grid
+        place-items-center
+        rounded-xl
+        cursor-pointer
+        transition
+        ease-out
+        hover:-translate-y-1 hover:scale-110
+        duration-200
+      "
+    >
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        :stroke="buttonColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+        ></path>
+      </svg>
+    </div>
+    <div
+      class="
+        text-white
+        bg-neutral-800
+        dark:bg-gray-50
+        w-14
+        h-14
+        mb-4
+        grid
+        place-items-center
+        rounded-xl
+        cursor-pointer
+        transition
+        ease-out
+        hover:-translate-y-1 hover:scale-110
+        duration-200
+      "
+    >
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        :stroke="buttonColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M21 13.255A23.931 23.931 0 0112 15c-3.183 0-6.22-.62-9-1.745M16 6V4a2 2 0 00-2-2h-4a2 2 0 00-2 2v2m4 6h.01M5 20h14a2 2 0 002-2V8a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        ></path>
+      </svg>
+    </div>
+    <div
+      class="
+        text-white
+        bg-neutral-800
+        dark:bg-gray-50
+        w-14
+        h-14
+        mb-4
+        grid
+        place-items-center
+        rounded-xl
+        cursor-pointer
+        transition
+        ease-out
+        hover:-translate-y-1 hover:scale-110
+        duration-200
+      "
+    >
+      <svg
+        class="w-6 h-6"
+        fill="none"
+        :stroke="buttonColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+        ></path>
+      </svg>
+    </div>
+    <div
+      class="
+        text-white
+        bg-neutral-800
+        dark:bg-gray-50
+        w-14
+        h-14
+        grid
+        place-items-center
+        rounded-xl
+        cursor-pointer
+        transition
+        ease-out
+        hover:-translate-y-1 hover:scale-110
+        duration-200
+      "
+    >
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        :stroke="buttonColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path
+          d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+        ></path>
+      </svg>
+    </div>
+    <div
+      class="
+        text-white
+        bg-neutral-800
+        dark:bg-gray-50
+        w-14
+        h-14
+        grid
+        place-items-center
+        rounded-xl
+        cursor-pointer
+        transition
+        ease-out
+        hover:translate-y-1 hover:scale-110
+        duration-200
+        mt-auto
+      "
+      @click="setTheme"
+    >
+      <svg
+        v-if="buttonColor === 'white'"
+        class="w-6 h-6"
+        fill="none"
+        :stroke="buttonColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+        ></path>
+      </svg>
+      <svg
+        v-else
+        class="w-6 h-6"
+        fill="none"
+        :stroke="buttonColor"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
+        ></path>
+      </svg>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: "Links",
+  data() {
+    return {
+      buttonColor: "",
+    };
+  },
+  methods: {
+    setTheme() {
+      if (localStorage.theme === "dark") {
+        localStorage.theme = "light";
+        this.buttonColor = "white";
+      } else {
+        localStorage.theme = "dark";
+        this.buttonColor = "#1C1C1C";
+      }
+      this.checkTheme();
+    },
+    checkTheme() {
+      if (
+        localStorage.theme === "dark" ||
+        (!("theme" in localStorage) &&
+          window.matchMedia("(prefers-color-scheme: dark)").matches)
+      ) {
+        document.documentElement.classList.add("dark");
+        this.buttonColor = "#1C1C1C";
+      } else {
+        document.documentElement.classList.remove("dark");
+        this.buttonColor = "white";
+      }
+    },
+  },
+  mounted() {
+    // Dark mode preference using localStorage
+    this.checkTheme();
+
+    // Whenever the user explicitly chooses to respect the OS preference
+    // localStorage.removeItem("theme");
+  },
+};
+</script>
+
+<style scoped>
+nav {
+  /* background: linear-gradient(220.55deg, #ff896d 0%, #d02020 100%); */
+  /* background: linear-gradient(220.55deg, #565656 0%, #353535 100%); */
+  background: #353535;
+}
+</style>
