f7209b751caa0025078e3e684966960b3a145096
daviddrob <david.drobnak26@gmail.com>
2022-02-20 17:09:05 +0100

    fix: clean up Project.vue

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -13,117 +13,41 @@
   >
     <h1 class="text-6xl mb-12 font-bold dark:text-glow font-serif">Projects</h1>
     <div id="hero" class="font-serif w-full overflow-x-hidden">
-      <div
-        class="
-          flex flex-col
-          justify-between
-          items-center
-          w-3/4
-          mb-28
-          bg-neutral-200
-          text-neutral-700
-          dark:bg-neutral-700
-          rounded-xl
-          dark:text-white
-        "
-      >
-        <ProjectItem
-          title="Clean Tab"
-          description="Clean Tab is a chromium extension that allows you to style a new tab in your chromium browser. It also shows the current weather in you area."
-          :techs="['Vue.JS']"
-          github="https://github.com/DavidDrob/clean-tab"
-          demo="https://github.com/DavidDrob/clean-tab"
-        />
-        <img
-          src="../assets/clean-tab.png"
-          alt="demo image"
-          class="w-full rounded-b-xl"
-        />
-      </div>
+      <ProjectItem
+        title="Clean Tab"
+        description="Clean Tab is a chromium extension that allows you to style a new tab in your chromium browser. It also shows the current weather in you area."
+        :techs="['Vue.JS']"
+        github="https://github.com/DavidDrob/clean-tab"
+        demo="https://github.com/DavidDrob/clean-tab"
+        image="clean-tab"
+      />
 
-      <div
-        class="
-          flex flex-col
-          justify-between
-          items-center
-          w-3/4
-          mb-28
-          bg-neutral-200
-          text-neutral-700
-          dark:bg-neutral-700
-          rounded-xl
-          dark:text-white
-        "
-      >
-        <ProjectItem
-          title="Faceit Bot"
-          description="This discord bot fetches the data about a users CS:GO statistics, like his/her region rank, win rate, statistics on a specific map, etc ... The bot uses the Faceit API to fetch data and can be added by anyone to their discord server for free."
-          :techs="['Node.JS']"
-          github="https://github.com/DavidDrob/faceit-discord-bot"
-          demo="https://top.gg/bot/816070908782575656"
-        />
-        <img
-          src="../assets/faceit-bot.png"
-          alt="demo image"
-          class="w-full h-fit rounded-b-xl"
-        />
-      </div>
+      <ProjectItem
+        title="Faceit Bot"
+        description="This discord bot fetches the data about a users CS:GO statistics, like his/her region rank, win rate, statistics on a specific map, etc ... The bot uses the Faceit API to fetch data and can be added by anyone to their discord server for free."
+        :techs="['Node.JS']"
+        github="https://github.com/DavidDrob/faceit-discord-bot"
+        demo="https://top.gg/bot/816070908782575656"
+        image="faceit"
+      />
 
-      <div
-        class="
-          flex flex-col
-          justify-between
-          items-center
-          w-3/4
-          mb-28
-          bg-neutral-200
-          text-neutral-700
-          dark:bg-neutral-700
-          rounded-xl
-          dark:text-white
-        "
-      >
-        <ProjectItem
-          title="Study App"
-          description="This (old) Single-page application I made allows you to study maths with the 10 provided problems. You can draw on the canvas, check if you have the correct solution, search for problems (from 0 to 9) or get a new random problem."
-          :techs="['Vue.JS']"
-          github="https://github.com/DavidDrob/study-app"
-          demo="https://daviddrob.github.io/study-app/"
-        />
-        <img
-          src="../assets/study-app.png"
-          alt="demo image"
-          class="w-full h-fit rounded-b-xl"
-        />
-      </div>
+      <ProjectItem
+        title="Study App"
+        description="This (old) Single-page application I made allows you to study maths with the 10 provided problems. You can draw on the canvas, check if you have the correct solution, search for problems (from 0 to 9) or get a new random problem."
+        :techs="['Vue.JS']"
+        github="https://github.com/DavidDrob/study-app"
+        demo="https://daviddrob.github.io/study-app/"
+        image="study-app"
+      />
 
-      <div
-        class="
-          flex flex-col
-          justify-between
-          items-center
-          w-3/4
-          mb-28
-          bg-neutral-200
-          text-neutral-700
-          dark:bg-neutral-700
-          rounded-xl
-          dark:text-white
-        "
-      >
-        <ProjectItem
-          title="Personal Portfolio"
-          description="It's the website you are looking at right now. I made this to brush up my UI design skills and to also have a website for presenting my projects in a simple and modern way."
-          :techs="['Vue.JS', 'TailwindCSS']"
-          github="https://github.com/DavidDrob/DavidDrob.github.io"
-          demo="/"
-        />
-        <img
-          src="../assets/portfolio.png"
-          alt="demo image"
-          class="w-full h-fit rounded-b-xl"
-        />
-      </div>
+      <ProjectItem
+        title="Personal Portfolio"
+        description="It's the website you are looking at right now. I made this to brush up my UI design skills and to also have a website for presenting my projects in a simple and modern way."
+        :techs="['Vue.JS', 'TailwindCSS']"
+        github="https://github.com/DavidDrob/DavidDrob.github.io"
+        demo="/"
+        image="portfolio"
+      />
     </div>
   </div>
 </template>
--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -1,71 +1,87 @@
 <template>
-  <aside class="pl-4 w-full mb-4">
-    <p class="text-2xl font-bold mt-4">
-      {{ title }}
-    </p>
-    <p class="mt-2 w-3/4">{{ description }}</p>
-    <hr class="mt-4 mb-2" />
-    <ul class="flex items-center w-3/4 flex-wrap lg:flex-nowrap">
-      <div class="flex" v-for="(tech, index) in techs" :key="index">
-        <li
-          class="shadow-lg px-3 py-1 rounded-xl mr-3 lg:mb-0 my-1"
-          :class="techColorMap[tech]"
-        >
-          {{ tech }}
-        </li>
-      </div>
-      <div class="flex lg:justify-around lg:items-center lg:ml-auto mr-6">
-        <li>
-          <a :href="github" target="_blank">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              :stroke="buttonColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="w-6 h-6 mr-3"
-            >
-              <path
-                d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
-              ></path>
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a :href="demo" target="_blank">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              :stroke="buttonColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="w-6 h-6"
-            >
-              <circle cx="12" cy="12" r="10"></circle>
-              <line x1="2" y1="12" x2="22" y2="12"></line>
-              <path
-                d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
-              ></path>
-            </svg>
-          </a>
-        </li>
-      </div>
-    </ul>
-  </aside>
+  <div
+    class="
+      flex flex-col
+      justify-between
+      items-center
+      w-3/4
+      mb-28
+      bg-neutral-200
+      text-neutral-700
+      dark:bg-neutral-700
+      rounded-xl
+      dark:text-white
+    "
+  >
+    <aside class="pl-4 w-full mb-4">
+      <p class="text-2xl font-bold mt-4">
+        {{ title }}
+      </p>
+      <p class="mt-2 w-3/4">{{ description }}</p>
+      <hr class="mt-4 mb-2" />
+      <ul class="flex items-center w-3/4 flex-wrap lg:flex-nowrap">
+        <div class="flex" v-for="(tech, index) in techs" :key="index">
+          <li
+            class="shadow-lg px-3 py-1 rounded-xl mr-3 lg:mb-0 my-1"
+            :class="techColorMap[tech]"
+          >
+            {{ tech }}
+          </li>
+        </div>
+        <div class="flex lg:justify-around lg:items-center lg:ml-auto mr-6">
+          <li>
+            <a :href="github" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                :stroke="buttonColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-6 h-6 mr-3"
+              >
+                <path
+                  d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"
+                ></path>
+              </svg>
+            </a>
+          </li>
+          <li>
+            <a :href="demo" target="_blank">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                :stroke="buttonColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="w-6 h-6"
+              >
+                <circle cx="12" cy="12" r="10"></circle>
+                <line x1="2" y1="12" x2="22" y2="12"></line>
+                <path
+                  d="M12 2a15.3 15.3 0 0 1 4 10 15.3 15.3 0 0 1-4 10 15.3 15.3 0 0 1-4-10 15.3 15.3 0 0 1 4-10z"
+                ></path>
+              </svg>
+            </a>
+          </li>
+        </div>
+      </ul>
+    </aside>
+    <img :src="imageUrl" :alt="image" class="w-full h-fit rounded-b-xl" />
+  </div>
 </template>
 
 <script>
 export default {
   name: "Project",
-  props: ["title", "description", "techs", "imageName", "github", "demo"],
+  props: ["title", "description", "techs", "image", "github", "demo"],
   data() {
     return {
       techColorMap: {
@@ -81,6 +97,11 @@ export default {
       if (localStorage.theme === "dark") this.buttonColor = "white";
       else if (localStorage.theme === "light") this.buttonColor = "#1C1C1C";
     }, 10);
+  },
+  computed: {
+    imageUrl() {
+      return new URL(`../assets/${this.image}.png`, import.meta.url).href;
+    },
   },
 };
 </script>
